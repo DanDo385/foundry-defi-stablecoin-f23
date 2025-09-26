175fcdd172b38de4e62b671c9fb7df2b90db5e8c
Dan Magro <dando385@yahoo.com>
2025-09-26 16:58:03 -0400

    first contract decentralizedstablecoin.sol complete

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -2,6 +2,39 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DecentralizedStableCoin {
-    constructor() ERC20("DecentralizedStableCoin", "DSC") {
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+
+    error DecentralizedStableCoin__AmountMustBeGreaterThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount == 0) {
+            revert DecentralizedStableCoin__AmountMustBeGreaterThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount == 0) {
+            revert DecentralizedStableCoin__AmountMustBeGreaterThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
