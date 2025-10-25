8549eaccd8de580a79df1378beb9d2e44b73a0a5
Dan Magro <dando385@yahoo.com>
2025-10-25 18:08:58 -0400

    finish dsc functions

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -7,18 +7,24 @@ pragma solidity ^0.8.18;
  * @notice This contract is the main contract for the DSCEngine system.
  * It is responsible for minting and burning DSC tokens, and for managing the
  * collateral and debt of the system.
+ * @notice A-B-O Aways be Overcollateralized!
  * @notice This contrat is VERY loosely based on the MakerDAO DAI engine.
  */
 
 contract DSCEngine {
     function depositCollateralAndMintDSC() external {}
 
+    function depositCollateral() external {}
+
     function redeemCollateralForDSC() external {}
 
-    function burnDSC() external {}
+    function redeemCollateral() external {}
 
+    function mintDSC() external {}
+
+    function burnDSC() external {}
+    
     function liquidate() external {}
     
     function getHealthFactor() external view returns (uint256) {}
-    
 }
